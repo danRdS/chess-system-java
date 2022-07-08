273811435f54ea22ba56e7c5c7798f816b93d812
danRdS <danielrdsilva@gmail.com>
2022-07-07 23:07:20 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -51,6 +51,24 @@ public class Board {
 		piece.position= position;
 	}
 	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		/*Neste 'if', se a peça estiver numa posição que está vazia(null), significa que não tem nenhuma peça nessa posição. Então retorna o 
+		 * valor nulo. Se não acontecer, ai faz o procedimento de retirar a peça do tabuleiro.*/
+		
+		Piece aux = piece(position);	/*A variável 'aux' vai receber uma peça que está nesta posição*/
+		aux.position = null; 			/*peça retirada do tabuleiro. Essa peça não tem posição mais.*/
+		pieces[position.getRow()][position.getColumn()] = null;	/*Essa matriz de peças nesta linha e coluna. Essa matriz de peças na posição
+		onde estou removendo a peça, agora vai ser nulo, indicando que não tem mais peça nessa posição.*/
+		
+		return aux; 					/*retorna a variável que contém a peça que foi retirada.*/
+	}
+	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -65,4 +83,5 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+	
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -23,6 +25,32 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	/*método pra tirar uma peça da posição de origem e colocá-la na posição de destino*/
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		/*converter as duas posições para posições da matriz*/
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		/*Antes de fazer um movimento, devo verificar se na posição de origem(source) havia uma peça*/
+		validateSourcePosition(source);	/*operação responsável por validar essa posição de origem, senão existir ela lançará uma exceção*/
+		Piece capturedPiece = makeMove(source, target); /*makeMove é uma operação responsável por fazer o movimento da peça*/
+		return (ChessPiece)capturedPiece; /*Downcasting pq 'capturedPiece' era do tipo 'Piece'*/
+	}
+	
+	private Piece makeMove(Position source, Position target) {
+		Piece p = board.removePiece(source);	/*removendo a peça que estava na posição de origem*/
+		Piece capturePiece = board.removePiece(target); /*remove a possivel peça que está na posição de destino e ela, por padrão, 
+		será a peça capturada.*/
+		board.placePiece(p, target);			/*colocando a peça 'p' na posição de destino*/
+		return capturePiece;
+	}
+	
+	private void validateSourcePosition(Position position) {
+		/*Se não existir uma peça nessa posição, vai retornar um erro*/
+		if(!board.thereIsAPiece(position)) {
+			throw new ChessException("There is no piece on source position");
+		}
+	}
+	
 	/*instancia as peças de xadrez informando as coordenadas no sistema do tabuleiro do xadrez e não da matriz pra não deixar confuso*/
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
@@ -43,4 +71,5 @@ public class ChessMatch {
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
+		
 }
