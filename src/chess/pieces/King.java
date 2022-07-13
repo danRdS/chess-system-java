ba6164ed10d4c05513cb169ccbaaae094ccf1a5b
danRdS <danielrdsilva@gmail.com>
2022-07-13 00:09:27 -0300

    Special move - Castling

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece{
 
-	public King(Board board, Color color) {
+	private ChessMatch chessMatch; /*porque o Rei precisa ter acesso a partida por causa da jogada especial 'Roque(Castling)'*/
+	
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
@@ -22,6 +26,12 @@ public class King extends ChessPiece{
 		/*verificar se essa peça p é nula ou se é uma peça adversária(se a cor da peça for diferente da do rei). No dois casos
 		 * poderá mover a peça pra qq uma das posições. */
 		return p == null || p.getColor() != getColor();
+	}
+	
+	private boolean testRookCastling(Position position) {
+		/*testa se a torre está apta para o Castlings. Ela está apta qdo sua qtde de movimentos é igual a 0*/
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
 	}
 	
 	@Override
@@ -73,6 +83,41 @@ public class King extends ChessPiece{
 		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// #specialmove castling
+		if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+			/*O rei está apto ao castling desde que não tenha feito nenhum movimento e a partida não pode estar em check*/
+			
+			// #specialmove castling kingside rook
+			Position posT1 = new Position(position.getRow(), position.getColumn() + 3);/*posT1 = posição da torre 1, que fica a 3 casas
+			a direita do rei*/
+			if(testRookCastling(posT1)) {
+				/*testa se nessa posição tem uma torre apta pra castling*/
+				Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+					/*testa se as duas posições entre o rei e a torre da direita estão vazias*/
+					mat[position.getRow()][position.getColumn() + 2] = true;
+					/*se todas as condições forem verdadeiras, então o movimento do rei pra duas casas a direita pode 
+					 * ser feito(Castling pequeno)*/
+				}
+			}
+			
+			// specialmove castling queenside rook
+			Position posT2 = new Position(position.getRow(), position.getColumn() - 4);/*posT1 = posição da torre 1, que fica a 4 casas
+			a direita do rei*/
+			if(testRookCastling(posT2)) {
+				/*testa se nessa posição tem uma torre apta pra castling*/
+				Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+					/*testa se as três posições entre o rei e a torre da esquerda estão vazias*/
+					mat[position.getRow()][position.getColumn() - 2] = true;
+		
+				}
+			}
 		}
 		return mat;
 	}
